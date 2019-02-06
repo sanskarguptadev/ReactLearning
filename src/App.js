88b36ff4da0fd2cee65faf0ed212418a9efbe56d
Gupta, Sanskar <sanskar.gupta@accenture.com>
2019-02-06 11:29:26 +0530

    React components, state and props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Menu from './components/menu';
+import {DISHES} from './shared/dishes';
 
 class App extends Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/"> Resturant Confusion</NavbarBrand>
-          </div>
-        </Navbar>  
+      <div>
+        <Navbar color="faded" light>
+          <NavbarBrand className="mr-auto" href="/"> Resturant Confusion</NavbarBrand>
+        </Navbar>
+        <Menu dishes={this.state.dishes}></Menu>  
       </div>
     );
   }
